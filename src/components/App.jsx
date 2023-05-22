ed51bb62f23ca241d9ed33b5b016e31bced5f6a7
PyotrKlim <113846380+PyotrKlim@users.noreply.github.com>
2023-05-22 16:04:51 +0300

    alteration for storage in "localStorage"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,18 @@ import FilterContacts from './FilterContacts/FilterContacts';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    localStorage.getItem('contacts') &&
+      this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   formSubmitHandler = newContact => {
     const isInContacts = this.state.contacts.some(
@@ -56,6 +60,7 @@ export class App extends Component {
     return (
       <>
         <ContactForm onSubmit={this.formSubmitHandler} />
+
         <FilterContacts onChange={this.onFilterChange} value={filter} />
         {contacts.length > 0 && (
           <ContactsList
